f23ae6d72e29e6cf3b4005380bd0b84f53fc90c8
Johnkyle66 <jangkayld@gmail.com>
2023-08-10 10:30:30 +0800

    Changes

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::Vector2u modeSize(200, 200);
+  sf::RenderWindow window(sf::VideoMode(modeSize), "SFML works!");
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
 
